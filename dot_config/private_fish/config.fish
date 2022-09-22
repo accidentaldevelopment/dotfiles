d95ad00899f98d30a0b2f8d876108290e8c93989
Brian Faga <brian@accidentaldevelopment.com>
2022-09-22 08:41:41 -0400

    feat(fish): add fish config

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,23 @@
+set fish_greeting
+
+set -x GPG_TTY "$(tty)"
+set -x EDITOR nvim
+set -x PAGER less
+set -x HOMEBREW_NO_ANALYTICS 1
+
+if infocmp wezterm &> /dev/null
+  set -x TERM wezterm
+else
+end
+
+if status is-interactive
+    fish_vi_key_bindings
+
+    fish_add_path "~/.cargo/bin"
+    fish_add_path "$(brew --prefix)/bin" "$(brew --prefix)/sbin"
+    source "$(brew --prefix asdf )/libexec/asdf.fish"
+
+    source "$(brew --prefix fzf)/shell/key-bindings.fish" && fzf_key_bindings
+
+    starship init fish | source
+end
