da5b1cc867ef1278d66af99c6fd0ead5b414e49c
Brian Faga <brian@accidentaldevelopment.com>
2023-01-05 09:34:30 -0500

    chore(nvim): cleanup some treesitter code

--- dot_config/nvim/lua/plugin/treesitter.lua
+++ dot_config/nvim/lua/plugin/treesitter.lua
@@ -1,78 +1,61 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  lazy = false,
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-context',
-    -- TODO: Unmaintained. See if we can find a replacement.
-    'p00f/nvim-ts-rainbow',
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = 'BufReadPost',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-context',
+        config = true,
+      },
+      -- TODO: Unmaintained. See if we can find a replacement.
+      'p00f/nvim-ts-rainbow',
+    },
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'bash',
+          'help',
+          'json',
+          'lua',
+          'markdown',
+          'markdown_inline',
+          'regex',
+          'rust',
+          'vim',
+        }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+        ignore_install = { '' }, -- List of parsers to ignore installing
+        autopairs = {
+          enable = true,
+        },
+        highlight = {
+          enable = true, -- false will disable the whole extension
+          disable = { '' }, -- list of language that will be disabled
+          additional_vim_regex_highlighting = true,
+        },
+        indent = { enable = true, disable = { 'yaml' } },
+        context_commentstring = {
+          enable = true,
+          enable_autocmd = false,
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+        },
+      })
+
+      -- -- Folding options
+      -- vim.o.foldmethod = 'expr'
+      -- vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+      -- -- Don't fold everything at startup
+      -- vim.o.foldlevelstart = 99
+      -- vim.o.foldcolumn = 'auto:3'
+      -- vim.opt.fillchars:append({
+      --   foldopen = '▼',
+      --   -- foldsep = '|',
+      --   foldclose = '▶',
+      -- })
+    end,
   },
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'bash',
-        'help',
-        'json',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'regex',
-        'rust',
-        'vim',
-      }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-      sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-      ignore_install = { '' }, -- List of parsers to ignore installing
-      autopairs = {
-        enable = true,
-      },
-      highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { '' }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
-      },
-      indent = { enable = true, disable = { 'yaml' } },
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-      },
-    })
-
-    require('treesitter-context').setup({
-      patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-          'class',
-          'function',
-          'method',
-          -- 'for', -- These won't appear in the context
-          -- 'while',
-          -- 'if',
-          -- 'switch',
-          -- 'case',
-        },
-        rust = {
-          'match',
-        },
-      },
-    })
-
-    -- Folding options
-    vim.o.foldmethod = 'expr'
-    vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-    -- Don't fold everything at startup
-    vim.o.foldlevelstart = 99
-    vim.o.foldcolumn = 'auto:3'
-    vim.opt.fillchars:append({
-      foldopen = '▼',
-      -- foldsep = '|',
-      foldclose = '▶',
-    })
-  end,
 }
