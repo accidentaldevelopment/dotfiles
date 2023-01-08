965a02e6457239d35d27e2157b5fe9b657613989
Brian Faga <brian@accidentaldevelopment.com>
2023-01-07 22:15:45 -0500

    feat(fish): add some vim-ier key bindings

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,11 +8,17 @@ set -x HOMEBREW_NO_ANALYTICS 1
 if status is-interactive
     fish_vi_key_bindings
 
+    bind -M insert \cr history-pager
+    bind -M insert \ch backward-char
+    bind -M insert \cl forward-char
+    bind -M insert \ck up-or-search
+    bind -M insert \cj down-or-search
+
     fish_add_path "~/.cargo/bin"
     fish_add_path "$(brew --prefix)/bin" "$(brew --prefix)/sbin"
-    source "$(brew --prefix asdf)/libexec/asdf.fish"
 
-    source "$(brew --prefix fzf)/shell/key-bindings.fish" && fzf_key_bindings
+    # source "$(brew --prefix asdf)/libexec/asdf.fish"
+    # source "$(brew --prefix fzf)/shell/key-bindings.fish" && fzf_key_bindings
 
     starship init fish | source
     enable_transience
