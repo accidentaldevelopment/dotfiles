16114dc867dd2fcd3037ec2ec827da7df922fa1a
Brian Faga <brian@accidentaldevelopment.com>
2023-03-16 17:52:08 -0400

    chore(nvim): help key mapping

--- dot_config/nvim/lua/plugin/treesitter.lua
+++ dot_config/nvim/lua/plugin/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    event = 'BufReadPost',
+    event = { 'BufReadPost', 'BufNewFile' },
     dependencies = {
       {
         'nvim-treesitter/nvim-treesitter-context',
