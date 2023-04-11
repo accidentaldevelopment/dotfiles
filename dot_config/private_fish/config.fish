76b0b771a1def524686ef6cbbaa96722f3ebd669
Brian Faga <brian@accidentaldevelopment.com>
2023-04-11 07:50:48 -0400

    chore(fish): update some env vars

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,16 +2,26 @@ set -u fish_greeting
 
 set -x GPG_TTY "$(tty)"
 set -x EDITOR nvim
-set -x PAGER less
+# set -x PAGER less
 set -x MANPAGER 'nvim +Man!'
 set -x HOMEBREW_NO_ANALYTICS 1
 set -x CARGO_REGISTRIES_CRATES_IO_PROTOCOL sparse
+
+set -x LESS_TERMCAP_mb (tput bold; tput setaf red) # begin bold
+set -x LESS_TERMCAP_md (tput setaf 74) # begin bold
+set -x LESS_TERMCAP_me (tput sgr0) # begin bold
+set -x LESS_TERMCAP_us (tput smul; tput setaf 182) # begin underline
+set -x LESS_TERMCAP_ue (tput rmul; tput sgr0) # begin underline
+set -x LESS_TERMCAP_so (tput smso) # begin standout-mode - info box
+set -x LESS_TERMCAP_se (tput rmso; tput sgr0) # begin standout-mode - info box
 
 if status is-interactive
     fish_vi_key_bindings
 
     fish_add_path "$HOME/.cargo/bin"
     fish_add_path "$(brew --prefix)/bin" "$(brew --prefix)/sbin"
+
+    alias sr='ssh -l root'
 
     # source "$(brew --prefix asdf)/libexec/asdf.fish"
     # source "$(brew --prefix fzf)/shell/key-bindings.fish" && fzf_key_bindings
