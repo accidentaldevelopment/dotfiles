1fad8ae17050dea495078dfc768fe36fd560a886
Brian Faga <brian@accidentaldevelopment.com>
2023-11-15 20:50:31 -0500

    fix(nvim): remove unneeded treesitter settings
    Had some old probably copy-pasted treesitter configurations. They
    either did nothing, or broke highlighting. So this just removes them.

--- dot_config/nvim/lua/plugin/treesitter.lua
+++ dot_config/nvim/lua/plugin/treesitter.lua
@@ -23,14 +23,11 @@ return {
         'vimdoc',
       },
       sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-      ignore_install = { '' }, -- List of parsers to ignore installing
       autopairs = {
         enable = true,
       },
       highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { '' }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
+        enable = true,
       },
       indent = { enable = true, disable = { 'yaml' } },
       context_commentstring = {
