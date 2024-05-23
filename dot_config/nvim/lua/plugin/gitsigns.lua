f27223da850d14e614043c67531c44bd289e0582
Brian Faga <brian@accidentaldevelopment.com>
2024-05-23 12:30:10 -0400

    revert(nvim): move back to gitsigns
    `mini.git` and `mini.diff` are nice, but don't provide inline blame.
    
    refs: 37560be795a2662627a4a1605b4e242c207b93e9

--- dot_config/nvim/lua/plugin/gitsigns.lua
+++ dot_config/nvim/lua/plugin/gitsigns.lua
@@ -1,60 +1,39 @@
 return {
-  {
-    'lewis6991/gitsigns.nvim',
-    event = 'BufReadPre',
-    cond = false,
-    opts = {
-      numhl = true,
-      current_line_blame_opts = {
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-      },
-      current_line_blame_formatter_opts = {
-        relative_time = false,
-      },
-      yadm = {
-        enable = false,
-      },
-      on_attach = function(buffer)
-        local wk = require('which-key')
-
-        wk.register({
-          [']h'] = { '<cmd>Gitsigns nav_hunk next<cr>', 'Next hunk' },
-          [']H'] = { '<cmd>Gitsigns nav_hunk last<cr>', 'Last hunk' },
-          ['[h'] = { '<cmd>Gitsigns nav_hunk prev<cr>', 'Previous hunk' },
-          ['[H'] = { '<cmd>Gitsigns nav_hunk first<cr>', 'First hunk' },
-          ['<leader>gh'] = {
-            name = 'hunk',
-            r = { '<cmd>Gitsigns reset_hunk<cr>', 'Reset Hunk' },
-          },
-        }, {
-          buffer = buffer,
-        })
-
-        wk.register({
-          ['ih'] = { '<cmd>Gitsigns select_hunk<cr>', 'Select Hunk' },
-        }, {
-          mode = { 'o', 'x' },
-          buffer = buffer,
-        })
-      end,
+  'lewis6991/gitsigns.nvim',
+  event = 'BufReadPre',
+  opts = {
+    numhl = true,
+    current_line_blame_opts = {
+      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
     },
-  },
-  {
-    'echasnovski/mini.diff',
-    event = 'BufReadPre',
-    opts = {
-      view = {
-        style = 'sign',
-      },
-      options = {
-        wrap_goto = true,
-      },
+    current_line_blame_formatter_opts = {
+      relative_time = false,
     },
-  },
-  {
-    'echasnovski/mini-git',
-    main = 'mini.git',
-    event = 'BufReadPre',
-    opts = {},
+    yadm = {
+      enable = false,
+    },
+    on_attach = function(buffer)
+      local wk = require('which-key')
+
+      wk.register({
+        [']h'] = { '<cmd>Gitsigns nav_hunk next<cr>', 'Next hunk' },
+        [']H'] = { '<cmd>Gitsigns nav_hunk last<cr>', 'Last hunk' },
+        ['[h'] = { '<cmd>Gitsigns nav_hunk prev<cr>', 'Previous hunk' },
+        ['[H'] = { '<cmd>Gitsigns nav_hunk first<cr>', 'First hunk' },
+        ['<leader>gh'] = {
+          name = 'hunk',
+          r = { '<cmd>Gitsigns reset_hunk<cr>', 'Reset Hunk' },
+        },
+      }, {
+        buffer = buffer,
+      })
+
+      wk.register({
+        ['ih'] = { '<cmd>Gitsigns select_hunk<cr>', 'Select Hunk' },
+      }, {
+        mode = { 'o', 'x' },
+        buffer = buffer,
+      })
+    end,
   },
 }
--- dot_config/nvim/lua/plugin/heirline/components/git.lua
+++ dot_config/nvim/lua/plugin/heirline/components/git.lua
@@ -2,51 +2,61 @@ local conditions = require('heirline.conditions')
 
 --- Git status. Shows branch name and status counts.
 return {
-  condition = function()
-    return vim.b.minidiff_summary
-  end,
+  condition = conditions.is_git_repo,
   init = function(self)
     ---@diagnostic disable-next-line: undefined-field - This variable exists as long as the above condition is true.
-    self.status_dict = vim.b.minidiff_summary
-    self.has_changes = ((self.status_dict.add ~= 0) or (self.status_dict.delete ~= 0) or (self.status_dict.change ~= 0))
+    self.status_dict = vim.b.gitsigns_status_dict
+    self.has_changes = (
+      (self.status_dict.added ~= 0)
+      or (self.status_dict.removed ~= 0)
+      or (self.status_dict.changed ~= 0)
+    )
   end,
   hl = 'SLGitBranch',
   {
     provider = function(self)
       -- return ('\239\144\152 ' .. self.status_dict.head)
-      if vim.b.minigit_summary ~= nil then
-        return (' ' .. vim.b.minigit_summary.head_name)
-      else
-        return ''
-      end
+      return (' ' .. self.status_dict.head)
     end,
     hl = { bold = true },
   },
   {
+    condition = function(self)
+      return self.has_changes
+    end,
     provider = '(',
   },
   {
     provider = function(self)
-      local count = (self.status_dict.add or 0)
-      return ('+' .. count)
+      local count = (self.status_dict.added or 0)
+      if count > 0 then
+        return ('+' .. count)
+      end
     end,
-    hl = 'MiniDiffSignAdd',
+    hl = 'GitSignsAdd',
   },
   {
     provider = function(self)
-      local count = (self.status_dict.delete or 0)
-      return ('-' .. count)
+      local count = (self.status_dict.removed or 0)
+      if count > 0 then
+        return ('-' .. count)
+      end
     end,
-    hl = 'MiniDiffSignDelete',
+    hl = 'GitSignsDelete',
   },
   {
     provider = function(self)
-      local count = (self.status_dict.change or 0)
-      return ('~' .. count)
+      local count = (self.status_dict.changed or 0)
+      if count > 0 then
+        return ('~' .. count)
+      end
     end,
-    hl = 'MiniDiffSignChange',
+    hl = 'GitSignsChange',
   },
   {
+    condition = function(self)
+      return self.has_changes
+    end,
     provider = ')',
   },
 }
