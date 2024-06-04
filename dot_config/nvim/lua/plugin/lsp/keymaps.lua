6a87e7c391dc2592b49ac9dbbc4914f817c8ab54
Brian Faga <brian@accidentaldevelopment.com>
2024-06-04 10:15:25 -0400

    feat(nvim): fix some deprecations and default keymaps
    Default LSP keymaps have been added, so start using those.

--- dot_config/nvim/lua/plugin/lsp/keymaps.lua
+++ dot_config/nvim/lua/plugin/lsp/keymaps.lua
@@ -4,74 +4,54 @@ local M = {}
 ---@param client vim.lsp.Client LSP Client object
 ---@param buffer number Buffer number
 function M.on_attach(client, buffer)
-  local util = require('util')
-
-  local function a(i) end
-
   require('which-key').register({
     buffer = buffer,
     ['<leader>l'] = {
       name = 'LSP',
-      I = { '<cmd>LspInstallInfo<cr>', 'Installer Info' },
-      S = { util.lazy_require('telescope.builtin', 'lsp_dynamic_workspace_symbols'), 'Workspace Symbols' },
-      i = { '<cmd>LspInfo<cr>', 'Info' },
+      I = { '<CMD>LspInstallInfo<CR>', 'Installer Info' },
+      S = { '<CMD>Telescope lsp_dynamic_workspace_symbols<CR>', 'Workspace Symbols' },
+      i = { '<CMD>LspInfo<CR>', 'Info' },
       l = { vim.lsp.codelens.run, 'CodeLens Action' },
-      n = { '<cmd>Navbuddy<cr>', 'Show Navbuddy' },
+      n = { '<CMD>Navbuddy<CR>', 'Show Navbuddy' },
       q = { vim.lsp.diagnostic.set_loclist, 'Quickfix' },
-      s = { util.lazy_require('telescope.builtin', 'lsp_document_symbols'), 'Document Symbols' },
-    },
-    ['<leader>c'] = {
-      name = 'Code Actions',
-      a = { vim.lsp.buf.code_action, 'Code Action', mode = { 'n', 'v' } },
-      A = {
-        function()
-          vim.lsp.buf.code_action({
-            context = {
-              only = {
-                'source',
-              },
-              diagnostics = {},
-            },
-          })
-        end,
-        'Source Action',
-      },
-      r = { vim.lsp.buf.rename, 'Rename', cond = client.supports_method(vim.lsp.protocol.Methods.textDocument_rename) },
+      s = { '<CMD>Telescope lsp_document_symbols<CR>', 'Document Symbols' },
     },
     ['<localleader>'] = {
-      n = { '<cmd>Navbuddy<cr>', 'Show Navbuddy' },
-      i = { '<cmd>LspInfo<cr>', 'Info' },
+      n = { '<CMD>Navbuddy<cr>', 'Show Navbuddy' },
+      i = { '<CMD>LspInfo<cr>', 'Info' },
     },
     g = {
       name = '+goto',
-      d = { '<cmd>Telescope lsp_definitions<cr>', 'Goto Definition' },
-      r = { '<cmd>Telescope lsp_references<cr>', 'References' },
-      D = { '<cmd>Telescope lsp_declarations<CR>', 'Goto Declaration' },
-      I = { '<cmd>Telescope lsp_implementations<CR>', 'Goto Implementation' },
-      t = { '<cmd>Telescope lsp_type_definitions<cr>', 'Goto Type Definition' },
+      d = { '<CMD>Telescope lsp_definitions<CR>', 'Goto Definition' },
+      D = { '<CMD>Telescope lsp_declarations<CR>', 'Goto Declaration' },
+      I = { '<CMD>Telescope lsp_implementations<CR>', 'Goto Implementation' },
+      t = { '<CMD>Telescope lsp_type_definitions<CR>', 'Goto Type Definition' },
     },
-    ['<C-s>'] = {
-      vim.lsp.buf.signature_help,
-      'Signature Help',
-      cond = client.supports_method(vim.lsp.protocol.Methods.textDocument_signatureHelp),
-      mode = { 'n', 'i' },
+    gr = {
+      r = { '<CMD>Telescope lsp_references<CR>', 'LSP References' },
     },
-    ['[d'] = { util.lazy(vim.diagnostic.goto_prev), 'Previous diagnostic' },
-    [']d'] = { util.lazy(vim.diagnostic.goto_next), 'Next diagnostic' },
     ['[e'] = {
-      util.lazy(vim.diagnostic.goto_prev, { severity = vim.diagnostic.severity.ERROR }),
+      function()
+        vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR })
+      end,
       'Previous Error',
     },
     [']e'] = {
-      util.lazy(vim.diagnostic.goto_next, { severity = vim.diagnostic.severity.ERROR }),
+      function()
+        vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR })
+      end,
       'Next Error',
     },
     ['[w'] = {
-      util.lazy(vim.diagnostic.goto_prev, { severity = vim.diagnostic.severity.WARNING }),
+      function()
+        vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.WARN })
+      end,
       'Previous Warning',
     },
     [']w'] = {
-      util.lazy(vim.diagnostic.goto_next, { severity = vim.diagnostic.severity.WARNING }),
+      function()
+        vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.WARN })
+      end,
       'Next Warning',
     },
   })
--- dot_config/nvim/lua/util.lua
+++ dot_config/nvim/lua/util.lua
@@ -1,28 +1,5 @@
 local M = {}
 
---- Return a function that calls `fn` with `...` as args.
---- @param fn function The function to call
---- @param ... any arguments passed to `fn`
---- @treturn function A function that calls `fn` with the given `args`.
-function M.lazy(fn, ...)
-  local args = { ... }
-  return function()
-    fn(unpack(args))
-  end
-end
-
---- Return a function that calls `fn` with `...` as args.
---- @param mod string Full path to module containing `fn`.
---- @param fn string The function to call
---- @param ... any arguments passed to `fn`
---- @return function A function that calls `fn` with the given `args`.
-function M.lazy_require(mod, fn, ...)
-  local args = { ... }
-  return function()
-    require(mod)[fn](unpack(args))
-  end
-end
-
 ---@param silent boolean?
 ---@param values? {[1]:any, [2]:any}
 function M.toggle(option, silent, values)
