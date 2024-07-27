621f9fc8076711288decd938b6f8567ca14b8288
Brian Faga <brian@accidentaldevelopment.com>
2024-07-26 22:19:05 -0400

    feat(nvim): add rustaceanvim
    Remove standard RA config and use rustaceanvim instead.

---
+++ dot_config/nvim/after/ftplugin/rust.lua
@@ -0,0 +1,6 @@
+local bufnr = vim.api.nvim_get_current_buf()
+
+vim.keymap.set('n', 'gra', function()
+  vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
+  -- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr })
--- dot_config/nvim/lua/plugin/dev/rust.lua
+++ dot_config/nvim/lua/plugin/dev/rust.lua
@@ -1,4 +1,47 @@
+local ra_config = {
+  check = {
+    command = 'clippy',
+  },
+  inlayHints = {
+    bindingModeHints = { enable = true },
+    closureReturnTypeHints = { enable = true },
+    expressionAdjustmentHints = {
+      enable = true,
+      hideOutsideUnsafe = true,
+    },
+    lifetimeElisionHints = {
+      enable = 'always',
+      useParameterNames = true,
+    },
+  },
+}
+
 return {
+  {
+    'neovim/nvim-lspconfig',
+    opts = function(_, opts)
+      opts.handlers.rust_analyzer = {}
+    end,
+  },
+  {
+    'mrcjkb/rustaceanvim',
+    ft = 'rust',
+    opts = {
+      tools = {
+        float_win_config = {
+          border = 'rounded',
+        },
+      },
+      server = {
+        default_settings = {
+          ['rust-analyzer'] = ra_config,
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = opts
+    end,
+  },
   {
     'nvim-treesitter',
     opts = function(_, opts)
