6b667742f01151ad7ac70b1e944611313107765c
Brian Faga <brian@accidentaldevelopment.com>
2024-08-01 16:09:15 -0400

    fix(nvim): resolve some issues in crates.nvim config
    `crates.nvim` was double configured to use builtin completion and LSP
    completion. There was also an issue with multiple hover windows that has
    been resolved.

--- dot_config/nvim/lua/plugin/dev/rust.lua
+++ dot_config/nvim/lua/plugin/dev/rust.lua
@@ -39,20 +39,22 @@ return {
       popup = {
         border = 'rounded',
       },
-      completion = {
-        cmp = {
-          enabled = true,
-        },
-        crates = {
-          enabled = true,
-        },
-      },
       lsp = {
         enabled = true,
         name = 'crates',
         actions = true,
         completion = true,
-        hover = true,
+        on_attach = function(_, bufnr)
+          -- Override default K to show popup if available
+          vim.keymap.set('n', 'K', function()
+            local c = require('crates')
+            if c.popup_available() then
+              c.show_popup()
+            else
+              vim.lsp.buf.hover()
+            end
+          end, { buffer = bufnr, desc = 'Creates popup or hover' })
+        end,
       },
     },
   },
