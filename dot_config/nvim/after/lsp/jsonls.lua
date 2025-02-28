6acb213f385b6fd945b9781a03c1323d10522463
Brian Faga <brian@accidentaldevelopment.com>
2025-02-28 14:07:53 -0500

    refactor(nvim): remove unused function from jsonls config

--- dot_config/nvim/after/lsp/jsonls.lua
+++ dot_config/nvim/after/lsp/jsonls.lua
@@ -1,16 +1,9 @@
 vim.lsp.config('jsonls', {
+  --- @module "neoconf"
+  --- @type neoconf.
   settings = {
     json = {
       schemas = require('schemastore').json.schemas(),
-    },
-  },
-  setup = {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line('$'), 0 })
-        end,
-      },
     },
   },
 })
